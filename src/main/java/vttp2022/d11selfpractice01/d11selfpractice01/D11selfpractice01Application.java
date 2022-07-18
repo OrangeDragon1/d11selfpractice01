08dc2834377a8fabec812242b44770f36b07b2df
OrangeDragon1 <wcks095@gmail.com>
2022-07-18 23:25:34 +0800

    initial commit

---
+++ src/main/java/vttp2022/d11selfpractice01/d11selfpractice01/D11selfpractice01Application.java
@@ -0,0 +1,39 @@
+package vttp2022.d11selfpractice01.d11selfpractice01;
+
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@SpringBootApplication
+public class D11selfpractice01Application {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(D11selfpractice01Application.class, args);
+		SpringApplication app = new SpringApplication(D11selfpractice01Application.class);
+		
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+		
+		System.out.printf("\nApplication started on port %s\n", port);
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter log() {
+		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);
+		return logger;
+	}
+}
---
+++ src/test/java/vttp2022/d11selfpractice01/d11selfpractice01/D11selfpractice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.d11selfpractice01.d11selfpractice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D11selfpractice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
